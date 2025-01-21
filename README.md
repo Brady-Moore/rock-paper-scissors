4ade053a3d7f8c2b3e8b1ad3cd048bbe7779c05f
Bradoshado <bradoshado@gmail.com>
2025-01-21 18:35:38 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Rock, Paper, Scissors
+
+This game is currently run entirely in the browser console.
+
+Later updates will add a GUI.
---
+++ app.js
@@ -0,0 +1,80 @@
+let humanScore = 0;
+let computerScore = 0;
+let rounds = 0;
+
+const getHumanChoice = () => {
+  return prompt("Rock, Paper, or Scissors?").toLowerCase();
+};
+
+const getComputerChoice = () => {
+  const RNG = Math.random();
+  if (RNG < 0.34) {
+    return "rock";
+  } else if (RNG < 0.67) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+};
+
+const winRound = () => {
+  humanScore++;
+  rounds++;
+  console.log("You win!");
+  console.log(
+    `Player: ${humanScore} Computer: ${computerScore} Rounds played: ${rounds}`
+  );
+};
+
+const loseRound = () => {
+  computerScore++;
+  rounds++;
+  console.log("You lose!");
+  console.log(
+    `Player: ${humanScore} Computer: ${computerScore} Rounds played: ${rounds}`
+  );
+};
+
+const playRound = (humanChoice, computerChoice) => {
+  if (humanChoice === "rock") {
+    switch (computerChoice) {
+      case "rock":
+        console.log("It's a tie!");
+        break;
+      case "paper":
+        loseRound();
+        break;
+      case "scissors":
+        winRound();
+        break;
+    }
+  } else if (humanChoice === "paper") {
+    switch (computerChoice) {
+      case "rock":
+        winRound();
+        break;
+      case "paper":
+        console.log("It's a tie!");
+        break;
+      case "scissors":
+        loseRound();
+        break;
+    }
+  } else if (humanChoice === "scissors") {
+    switch (computerChoice) {
+      case "rock":
+        loseRound();
+        break;
+      case "paper":
+        winRound();
+        break;
+      case "scissors":
+        console.log("It's a tie!");
+        break;
+    }
+  }
+};
+
+while (rounds < 5) {
+  playRound(getHumanChoice(), getComputerChoice());
+}
